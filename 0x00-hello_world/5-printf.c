42c16fafce8000137668af165f8e810d115546ad
pherbish <ochiengpherbish2018@gmail.com>
2023-10-05 14:28:15 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: returns 0
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
