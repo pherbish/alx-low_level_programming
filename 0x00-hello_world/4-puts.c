d24b7ddf495f976dace5864243fd9bc8a0666003
pherbish <ochiengpherbish2018@gmail.com>
2023-10-05 14:09:39 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function printing output
+ * Return:its an integer returning 0 if succesfull
+ */
+
+int main(void)
+
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
