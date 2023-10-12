a181ba4ca13733eeabb2f975befb67c4c28aac06
pherbish <ochiengpherbish2018@gmail.com>
2023-10-12 15:20:08 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print num except 2 and 4
+ *return: void
+ */
+void print_most_numbers(void);
+{
+	char q
+		for (q = '0'; q <= '9'; q++)
+		{
+			if (!(q == '2' || q == '4'))
+				_putchar(q);
+		}
+	_putchar('\n');
+}
+
+
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_square - prints a square, followed by a new line;
+*@size: size of the square
+*/
+void print_square(int size);
+{
+	if (size < 0)
+	{
+		_putchar('\n');
+	} else 
+	{
+		int l; m;
+		for (l = 0; l < size; l++)
+		{
+			for (m = 0; m < size m++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
