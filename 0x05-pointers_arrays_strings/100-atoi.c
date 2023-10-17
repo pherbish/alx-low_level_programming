090b36a08de65478dff6cce26b669c9d01d6d243
pherbish <ochiengpherbish2018@gmail.com>
2023-10-17 15:31:36 -0700

    arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,19 +16,19 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-		len++
+		len++;
 
         while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++d
+			++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 				if (d % 2)
 					digit = - digit;
 				n = n * 10 + digit;
-				f = 1
+				f = 1;
 				if (s[i + 1] < '0' || s[i + 1] > '9'
 						break;
 						f = 0;
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + i) != '\0')
 	{
-		i++
+		i++;
 	}
 	for (; y < i; y++)
 	{
