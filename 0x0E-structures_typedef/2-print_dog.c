acebf7aa61f2c2aa226301aea74513dc534dfc2d
pherbish <ochiengpherbish2018@gmail.com>
2023-11-07 14:18:15 -0800

    correct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,9 +15,9 @@ void print_dog(struct dog *d)
 			printf("Name : %s\n", d->name);
 		printf("Age : %f\n", d->age);
 		
-		if (!((d->owner))
+		if (!((d->owner)))
 			printf("Owner : (nil)\n");
 		else
 		printf("Owner : %s\n", d->owner);
-		}
-		}
+	}
+}
