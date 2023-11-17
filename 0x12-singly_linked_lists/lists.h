3bab6b8f01bd3e909102630465aaa1abcedb6c4d
pherbish <ochiengpherbish2018@gmail.com>
2023-11-17 01:03:05 -0800

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,26 @@
 #ifndef LIST_H
 #define LIST_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ *struct list_s-stucture of linked list
+ *@str:string for malloc
+ *@len:length of list
+ *@next: nex pointer
+ *Description:singly linked list.
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void first(void);
-#endif
+#endif /*LINKED*/
