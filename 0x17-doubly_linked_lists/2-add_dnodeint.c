4eed229ca1b7f5978dcf1c6b98eec4bdf570e3f6
pherbish <pherbishorwa@gmail.com>
2024-01-12 01:18:27 -0800

    1st correction

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,38 +1,36 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
+ * add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
  */
-list_t *add_node(list_t **head, const char *str)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	dlistint_t *new;
+	dlistint_t *h;
 
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
 	{
-		free(new);
-		return (NULL);
+		while (h->prev != NULL)
+			h = h->prev;
 	}
 
-	for (len = 0; str[len];)
-		len++;
+	new->next = h;
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
+	if (h != NULL)
+		h->prev = new;
 
 	*head = new;
 
